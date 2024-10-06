d88125183ea745a49539c355819d2013b87b905a
Razen <09876sp54321@gmail.com>
2024-10-06 21:08:34 +0530

    deployment commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const isProjectSite = false
+const base = isProjectSite ? '/DiceGame/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })
